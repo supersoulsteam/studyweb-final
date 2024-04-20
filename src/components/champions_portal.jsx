a066e94053a29c03b32346b8696b41298bff9a2f
supersoulsteam <supersoulsteam@gmail.com>
2024-04-20 23:38:29 +0530

    semifinal_3

--- src/components/champions_portal.jsx
+++ src/components/champions_portal.jsx
@@ -7,8 +7,8 @@ import Navbar from "./navbar"
 const Champions_portal = () => {
   return (
     <div className="champion-main">
-      <Navbar/>
-        <div className="tcp">
+      <Navbar />
+      <div className="tcp">
         <h1>CHAMPIONS PORTAL</h1>
       </div>
       <div className="cp-main">
@@ -39,26 +39,27 @@ const Champions_portal = () => {
           <div className="cp-img-text-left">In AI mentor platforms, tensor processing is taught through theory, hands-on exercises, projects, and community support. Users learn tensor fundamentals, manipulate tensors using Python and libraries like TensorFlow, and apply them to real-world problems. Projects and challenges deepen understanding, while forums facilitate collaboration. Feedback mechanisms assess users' proficiency, guiding their learning journey.</div>
         </div>
         <div className="cp-feature-right">
-          <div className="cp-img-text-right">User-friendly interfaces in AI mentor platforms prioritize intuitive navigation, clear content organization, and interactive features. They offer easy access to learning resources such as tutorials, lectures, and exercises, with seamless integration of programming environments for hands-on practice. Personalized learning paths cater to individual needs, while progress tracking and feedback mechanisms enhance engagement.</div>
+          <div className="cp-img-text-right">User-friendly interfaces in AI mentor platforms prioritize intuitive navigation, clear content organization, and interactive features. They offer easy access to learning resources such as tutorials, lectures, and exercises, with seamless integration of programming environments for hands-on practice. Personalized learning paths cater to individual needs, while progress tracking and feedback mechanisms enhance engagement.
+          </div>
           <div className="cp-img-right">
             <img className="cp-image" src={e} alt="" />
           </div>
         </div>
       </div>
-      
+
       <div className="cp-advantage-main">
         <div className="c-citation-main">
-        <div className="c-citation-main-heading">
-          <h1>ADVANTAGES</h1>
-        </div>
-        <div className="c-citation-iframe-main">
-          <div className="c-citation-outer">
-            <iframe className="c-citation-iframe" src={s}></iframe>
+          <div className="c-citation-main-heading">
+            <h1>ADVANTAGES</h1>
+          </div>
+          <div className="c-citation-iframe-main">
+            <div className="c-citation-outer">
+              <iframe className="c-citation-iframe" src={s}></iframe>
+            </div>
           </div>
         </div>
       </div>
-      </div>
-        <Footer/>   
+      <Footer />
     </div>
   )
 }
